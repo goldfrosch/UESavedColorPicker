cb9e0f9082e6b8691e135741e4fea10a553e4f59
GoldFrosch <goldfrosch@daum.net>
2025-06-27 13:10:29 +0900

    feat: 플러그인 생성

---
+++ Plugins/SavedColorPicker/Source/SavedColorPicker/Public/SavedColorPicker.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "Modules/ModuleManager.h"
+
+class FSavedColorPickerModule : public IModuleInterface
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+
+	FName PropertyEditor;
+};
---
+++ Plugins/SavedColorPicker/Source/SavedColorPicker/SavedColorPicker.Build.cs
@@ -0,0 +1,32 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class SavedColorPicker : ModuleRules
+{
+	public SavedColorPicker(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				"Slate",
+				"DetailCustomizations",
+				"PropertyEditor",
+				"AppFramework"
+			}
+		);
+
+
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"CoreUObject",
+				"Engine",
+				"SlateCore"
+			}
+		);
+	}
+}
