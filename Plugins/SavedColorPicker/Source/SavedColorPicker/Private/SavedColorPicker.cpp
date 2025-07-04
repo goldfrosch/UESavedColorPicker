8761e217c80514ae5668d7cca0d3343ab3590a98
GoldFrosch <goldfrosch@daum.net>
2025-07-04 22:58:15 +0900

    feat: 리메이크를 위한 미사용 파일 전체 제거

--- Plugins/SavedColorPicker/Source/SavedColorPicker/Private/SavedColorPicker.cpp
+++ Plugins/SavedColorPicker/Source/SavedColorPicker/Private/SavedColorPicker.cpp
@@ -1,38 +1,14 @@
 ﻿#include "SavedColorPicker.h"
 
-#include "CustomizeSavedColorPalette.h"
 
 #define LOCTEXT_NAMESPACE "FSavedColorPickerModule"
 
 void FSavedColorPickerModule::StartupModule()
 {
-	PropertyEditor = "PropertyEditor";
-	FPropertyEditorModule& PropertyModule = FModuleManager::GetModuleChecked<
-		FPropertyEditorModule>(PropertyEditor);
-
-	PropertyModule.RegisterCustomPropertyTypeLayout(
-		NAME_LinearColor
-		, FOnGetPropertyTypeCustomizationInstance::CreateStatic(
-			&FCustomizeSavedColorPalette::MakeInstance));
-	PropertyModule.RegisterCustomPropertyTypeLayout(
-		NAME_Color
-		, FOnGetPropertyTypeCustomizationInstance::CreateStatic(
-			&FCustomizeSavedColorPalette::MakeInstance));
-
-	PropertyModule.NotifyCustomizationModuleChanged();
 }
 
 void FSavedColorPickerModule::ShutdownModule()
 {
-	if (FModuleManager::Get().IsModuleLoaded(PropertyEditor))
-	{
-		FPropertyEditorModule& PropertyModule = FModuleManager::GetModuleChecked
-			<FPropertyEditorModule>(PropertyEditor);
-		PropertyModule.UnregisterCustomPropertyTypeLayout(NAME_LinearColor);
-		PropertyModule.UnregisterCustomPropertyTypeLayout(NAME_Color);
-
-		PropertyModule.NotifyCustomizationModuleChanged();
-	}
 }
 
 #undef LOCTEXT_NAMESPACE
--- Plugins/SavedColorPicker/Source/SavedColorPicker/Public/SavedColorPicker.h
+++ Plugins/SavedColorPicker/Source/SavedColorPicker/Public/SavedColorPicker.h
@@ -9,4 +9,5 @@ public:
 	virtual void ShutdownModule() override;
 
 	FName PropertyEditor;
+	FName SlateColor;
 };
